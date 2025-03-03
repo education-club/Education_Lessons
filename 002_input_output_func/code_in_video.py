60fba74ec877eec91cd19bf873dbc6a55590f835
masterovoj <17942014@mail.ru>
2025-03-03 20:43:55 +0300

    Изменение второй части.

---
+++ 002_input_output_func/code_in_video.py
@@ -0,0 +1,14 @@
+print('\nФункции ввода-вывода.\n')
+
+# region {===== INPUT FUNCTION =====}
+
+
+
+# endregion {===== INPUT FUNCTION =====}
+
+
+# region {===== PRINT FUNCTION =====}
+
+
+
+# endregion {===== PRINT FUNCTION =====}
